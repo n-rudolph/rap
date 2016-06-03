ed4f61fb5d4b5eedb221ce5fba176df051a8f95e
Luciano Sartor <luciano.sartor@ing.austral.edu.ar>
2016-06-03 18:22:18 -0300

    Added Pikachu. Working on the attacks

---
+++ Assets/Scripts/ThunderSpere.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThunderSpere : MonoBehaviour {
+
+	public GameObject sphere;
+	public GameObject startPosition;
+	public GameObject endPosition;
+
+	private float speed = 1f;
+	// Use this for initialization
+	void Start () {
+		sphere.transform.position = startPosition.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sphere.transform.position = Vector3.MoveTowards (sphere.transform.position, endPosition.transform.position, speed * Time.deltaTime);
+		if (sphere.transform.position == endPosition.transform.position) {
+			sphere.transform.position = startPosition.transform.position;
+		}
+	}
+}
